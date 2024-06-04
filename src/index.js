3dbd4aa7ff35f710a10a6975a571c63cde74784d
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-06-04 18:28:36 +0100

    Completed for the most part

--- src/index.js
+++ src/index.js
@@ -1,98 +1,117 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
-const dogList = document.querySelector(".dogs-list");
-const dogContainer = document.querySelector(".main");
 
-function creatingCards () {
-for (let i = 0; i < data.length; i++) {
-  const dog = data[i];
+const state = {
+  dogs: data,
+  selectedDog: 0,
+};
+console.log(state);
 
-  //Creating dog list buttons:
-  const li = document.createElement("li");
-  li.classList = "dogs-list__button";
-  li.textContent = dog.name;
-  dogList.append(li);
+function addButton() {
+  const dogContainer = document.querySelector(".main");
+  const addButton = document.querySelector(
+    ".dogs-list__button",
+    ".dogs-list__button--add"
+  );
 
-  //Adding event listener:
-  li.addEventListener("click", function () {
-    console.log(dog.name);
+  createDogList();
 
-    //Clearing previous dog cards:
+  addButton.addEventListener("click", () => {
     dogContainer.innerHTML = " ";
-
-    //Creating new dog card:
-    const dogCard = document.createElement("section");
-    dogCard.classList = "main__dog-section";
-    dogContainer.append(dogCard);
-
-    //Adding heading to dog card:
-    const heading = document.createElement("h2");
-    heading.textContent = dog.name;
-    dogCard.append(heading);
-
-    //Adding the image:
-    const dogImage = document.createElement("img");
-    dogImage.src = dog.image;
-    dogCard.append(dogImage);
-
-    //Adding the description:
-    const dogDescription = document.createElement("div");
-    dogDescription.classList = "main__dog-section__desc";
-    dogCard.append(dogDescription);
-
-    //Adding heading to the description:
-    const bioHeading = document.createElement("h3");
-    bioHeading.textContent = "Bio";
-    dogDescription.append(bioHeading);
-
-    //Adding paragraph to the description:
-    const bioDescr = document.createElement("p");
-    bioDescr.textContent = dog.bio;
-    dogDescription.append(bioDescr);
-
-    //Adding 'is naughty?':
-    const isNaughty = document.createElement("p");
-    isNaughty.innerHTML = "<em>Is Naughty? </em>   Yes!";
-    dogCard.append(isNaughty);
-
-    //Adding button:
-    const button = document.createElement("button");
-    button.textContent = "Good dog!";
-    dogCard.append(button);
-
-    //Adding event listener to button:
-    button.addEventListener("click", function () {
-      button.textContent = "It works!";
-    });
+    const formCard = document.createElement("section");
+    formCard.classList = "main__dog-section";
+    dogContainer.append(formCard);
+    const formHeading = document.createElement("h2");
+    formHeading.textContent = "Add a new Dog";
+    formCard.append(formHeading);
+    createForm();
+    formCard.appendChild(createForm());
   });
 }
+function createDogList() {
+  const dogContainer = document.querySelector(".main");
+  const dogsList = document.querySelector(".dogs-list");
+
+  for (let i = 0; i < state.dogs.length; i++) {
+    const li = document.createElement("li");
+    li.className = "dogs-list__button";
+    li.innerText = state.dogs[i].name;
+    dogsList.append(li);
+
+    li.addEventListener("click", () => {
+      dogContainer.innerHTML = " ";
+      selectedDog = state.dogs[i];
+      console.log(selectedDog);
+      createDogCard(selectedDog);
+
+      dogContainer.appendChild(createDogCard(selectedDog));
+    });
+  }
 }
-
-console.log(dogContainer);
-
-const addButton = document.querySelector(
-  ".dogs-list__button",
-  ".dogs-list__button--add"
-);
-//Adding a function to the add button:
-addButton.addEventListener("click", function () {
-  dogContainer.innerHTML = "";
-
+function createDogCard(selectedDog) {
   //Creating new dog card:
   const dogCard = document.createElement("section");
   dogCard.classList = "main__dog-section";
-  dogContainer.append(dogCard);
+  createDogHeading(selectedDog);
+  createDogImage(selectedDog);
+  createDogDescription(selectedDog);
+  createIsNaughty();
+  createGoodDogButton();
+  dogCard.appendChild(createDogHeading(selectedDog));
+  dogCard.appendChild(createDogImage(selectedDog));
+  dogCard.appendChild(createDogDescription(selectedDog));
+  dogCard.appendChild(createIsNaughty());
+  dogCard.appendChild(createGoodDogButton());
+  return dogCard;
+}
 
-  //Adding heading to dog card:
+function createDogHeading(selectedDog) {
   const heading = document.createElement("h2");
-  heading.textContent = "Add a new Dog";
-  dogCard.append(heading);
+  heading.textContent = selectedDog.name;
+  return heading;
+}
 
+function createDogImage(selectedDog) {
+  const dogImage = document.createElement("img");
+  dogImage.src = selectedDog.image;
+  return dogImage;
+}
+
+function createDogDescription(selectedDog) {
+  const dogDescription = document.createElement("div");
+  dogDescription.classList = "main__dog-section__desc";
+
+  //Adding heading to the description:
+  const bioHeading = document.createElement("h3");
+  bioHeading.textContent = "Bio";
+  dogDescription.append(bioHeading);
+
+  //Adding paragraph to the description:
+  const bioDescr = document.createElement("p");
+  bioDescr.textContent = selectedDog.bio;
+  dogDescription.append(bioDescr);
+
+  return dogDescription;
+}
+
+function createIsNaughty() {
+  const isNaughty = document.createElement("p");
+  isNaughty.innerHTML = "<em>Is Naughty? </em>   Yes!";
+  return isNaughty;
+}
+
+function createGoodDogButton() {
+  //Adding button:
+  const button = document.createElement("button");
+  button.textContent = "Good dog!";
+  return button;
+}
+
+function createForm() {
   //Adding the form:
   const form = document.createElement("form");
   form.classList = "form";
-  dogCard.append(form);
 
   //Adding details to the form:
   const nameLabel = document.createElement("label");
@@ -128,31 +147,29 @@ addButton.addEventListener("click", function () {
   bioInput.setAttribute("name", "bio");
   form.append(bioInput);
 
-    const submit = document.createElement("input");
-    submit.setAttribute("type", "submit");
-    submit.setAttribute("id", "submit");
-    submit.setAttribute("name", "submit");
-    submit.setAttribute("value", "Let's add a dog!");
-    submit.setAttribute("class", "form__button");
-   form.append(submit);
+  const submit = document.createElement("input");
+  submit.setAttribute("type", "submit");
+  submit.setAttribute("id", "submit");
+  submit.setAttribute("name", "submit");
+  submit.setAttribute("value", "Let's add a dog!");
+  submit.setAttribute("class", "form__button");
+  form.append(submit);
 
-   form.addEventListener("submit", function(event) {
+  form.addEventListener("submit", function (event) {
     event.preventDefault();
-    
     const newDogObject = {};
-      newDogObject.name = nameInput.value
-      newDogObject.image = imageInput.value
-      newDogObject.bio = bioInput.value
-    
+    newDogObject.name = nameInput.value;
+    newDogObject.image = imageInput.value;
+    newDogObject.bio = bioInput.value;
+
     console.log(newDogObject);
 
-     data.push(newDogObject);
+    state.dogs.unshift(newDogObject);
+    console.log(state.dogs);
 
-     creatingCards(newDogObject);
-     console.log(data);
+    createDogList(state.dogs);
+  });
+  return form;
+}
 
-
-   });
-});
-
-creatingCards();
+addButton();
